5e42110220494ccd2104a60702195e34d2a274ea
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-11-08 12:13:58 +0900

    Can exit / separate command and arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self, Write};
+use std::process::exit;
 
 fn read_eval_print() {
     // Show prompt
@@ -9,8 +10,22 @@ fn read_eval_print() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).ok();
 
-    // Echo
-    print!("{}", input);
+    // Evaluate
+    let exit_commands = vec!["exit", "logout", "bye"];
+    let mut args = input.split_whitespace();
+    // Do nothing if an empty line is given
+    if let Some(command) = args.nth(0) {
+        // Exit
+        if exit_commands.contains(&command) {
+            println!("logout");
+            exit(0);
+        }
+        // Echo command and arguments
+        println!("{}", command);
+        for arg in args {
+            println!("{}", arg)
+        }
+    }
 }
 
 fn main() {
