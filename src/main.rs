a4260a46f9faf04176d17c0a997c199bae786ac3
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-11-08 11:54:12 +0900

    Read-Echo-Print-Loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io::{self, Write};
+
+fn read_eval_print() {
+    // Show prompt
+    print!("$ ");
+    io::stdout().flush().ok();
+
+    // Read line
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok();
+
+    // Echo
+    print!("{}", input);
+}
+
 fn main() {
-    println!("Hello, world!");
+    // REPL
+    loop {
+        read_eval_print();
+    }
 }
